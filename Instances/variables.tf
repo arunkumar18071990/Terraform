adc3a8a1bbd307c68adfe6494d481ec228c55ef6
Arun Kumar <109980875+arunkumar18071990@users.noreply.github.com>
2024-11-23 12:43:31 +0530

    Create variables.tf
    final

---
+++ Instances/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "ami" {
+  default = "ami-08bc77a2c7eb2b1da"
+}
+
+variable "key_name" {
+  default = "my-key-pair"
+}
